0618b5a5b533f1708383803017516caca4e3a152
rajiv jadon <sundersingh73273@gmail.com>
2026-03-28 18:49:04 +0530

    Add files via upload

---
+++ practise.py
@@ -0,0 +1,30 @@
+# def fibonacci(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+
+# n = int(input("Enter number of terms: "))
+
+# for i in range(n):
+#     print(fibonacci(i), end=" ")
+
+
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+# num = int(input("Enter a number: "))
+
+# if num < 0:
+#     print("Factorial is not defined for negative numbers")
+# else:
+#     print("Factorial of", num, "is", factorial(num))
+
+# what is single linked list and discus what is time complexity
+#
+#time space trado
